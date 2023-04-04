ab3f65c3141cd448e6db28ad7fb1f05dc83c3b7a
Higor Braga <higorbraga00@gmail.com>
2023-04-03 22:47:31 -0300

    remove e refatora componentes princias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NewTodo from './components/NewTodo';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodo = (value) => {
+    setTodos([
+      ...todos,
+      {
+        id: new Date().getTime(),
+        tittle: value,
+        checked: false,
+      },
+    ]);
+  };
+
+  const toggle = (todo) => {
+    setTodos(
+      todos.map((obj) =>
+        obj.id === todo.id ? { ...obj, checked: !todo.checked } : obj
+      )
+    );
+  };
+
+  const remove = (index) => {
+    const novoArray = [...todos];
+    novoArray.splice(index, 1);
+    setTodos(novoArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section id="app" className="container">
+      <header>
+        <h1 className="tittle">todo</h1>
       </header>
-    </div>
+      <section className="main">
+        <NewTodo onNewTodo={onNewTodo} />
+        <TodoList todos={todos} onToggle={toggle} onRemove={remove} />
+      </section>
+    </section>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
